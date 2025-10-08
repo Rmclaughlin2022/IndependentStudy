571a8d4bacf7241252ea83c9c9c694d40d6ff865
Rmclaughlin2022 <122641423+Rmclaughlin2022@users.noreply.github.com>
2025-10-08 01:06:28 -0500

    firebase fully connected
    added a button to test to see if its actually connected, and the data shows up on the website

--- tracking-app/app/(tabs)/index.tsx
+++ tracking-app/app/(tabs)/index.tsx
@@ -1,10 +1,47 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect, useState}  from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import { db } from "../firebaseConfig";
+import { collection, addDoc, getDocs, DocumentData } from "firebase/firestore";
+
+interface Tdata {
+  name: string;
+  age: string;
+  email: string;
+  password: string;
+  steps: number;
+  date: string; 
+}
 
 export default function App() {
+  const [data, setData] = useState<Tdata[]>([]);
+
+  async function addSample(): Promise<void> {
+    await addDoc(collection(db, "trackingData"), {
+      steps: Math.floor(Math.random() * 10000),
+      date: new Date().toISOString(),
+    });
+    fetchData();
+  }
+
+
+  async function fetchData(): Promise<void> {
+    const snapshot = await getDocs(collection(db, "trackingData"));
+    const items: Tdata[] = snapshot.docs.map((doc) => doc.data() as Tdata);
+    setData(items);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24 }}>🚀 Hello, React Native!</Text>
+      <Button title="Add Steps" onPress={addSample} />
+      {data.map((item, i) => (
+        <Text key={i}>
+          {item.date}: {item.steps} steps
+        </Text>
+      ))}
     </View>
   );
 }
@@ -12,8 +49,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
---
+++ tracking-app/app/firebaseConfig.js
@@ -0,0 +1,17 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAYxS5M2NyFFjGjabE7J8UKnCg3mHGRcU",
+  authDomain: "trackingapp-3474b.firebaseapp.com",
+  projectId: "trackingapp-3474b",
+  storageBucket: "trackingapp-3474b.firebasestorage.app",
+  messagingSenderId: "1636497827",
+  appId: "1:1636497827:web:a71cf4f1cd3a8ed9390216",
+  measurementId: "G-K3C6YKRRCV",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
